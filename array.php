7ef889e97f955279f3928eeef9a78c4c40dc6d81
khushi-vadera <148628587+khushi-vadera@users.noreply.github.com>
2024-01-23 18:43:55 +0530

    array

---
+++ array.php
@@ -0,0 +1,10 @@
+<?php
+$b2=array("b2","atmiya","unversity","class");
+
+echo $b2[3];
+echo "<br>";
+print_r($b2);
+
+
+
+?>
---
+++ arrayy.php
@@ -0,0 +1,12 @@
+<?php
+$b2=array("b2","atmiya","unversity","class","student","name");
+
+foreach($b2 as $mytempl)
+{
+	echo $mytempl;
+	echo "<br>";
+}
+
+
+
+?>
