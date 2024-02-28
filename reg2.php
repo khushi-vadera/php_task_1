51921680c88d157f5f0a478be6d533c6195b4180
khushi-vadera <148628587+khushi-vadera@users.noreply.github.com>
2024-02-28 08:11:28 +0530

    reg

---
+++ reg2.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<body>
+	<form>
+		username:<input type="text" name="unm"/><br><br>
+		password:<input type="text" name="pws"/><br><br>
+		First Name:<input type="text" name="fnm"/><br><br>
+		Last Name:<input type="text" name="lnm"/><br><br>
+		Email:<input type="text" name="em"/><br><br>
+		Moblie No.:<input type="text" name="mno"/><br><br>
+		Address.:<input type="text" name="add"/><br><br>
+		submit:<input type="submit" name="submit"/><br><br>
+	</form>
+</body>
+</head>
+</html>
